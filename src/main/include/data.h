ae40df639ccac53037afc954a8a68e3c73e89053
Luis Miguel Mejía Suárez <lmejias@psl.com.co>
2019-04-10 13:41:05 -0500

    Adding the first version of the Particle & Vector stucts

---
+++ src/main/include/data.h
@@ -0,0 +1,26 @@
+#pragma once
+
+/**
+ * Represents a circular shaped particle in a two-dimensional space.
+ */
+typedef struct {
+  double x;
+  double y;
+  double radious;
+} Particle;
+
+/**
+ * Physical properties of a particle,
+ * used in the forces and acceleration computations.
+ */
+typedef struct {
+  double mass;
+} ParticleProperties;
+
+/**
+ * Represents a vector in a two-dimensional space.
+ */
+typedef struct {
+  double x_component;
+  double y_component;
+} Vector;
