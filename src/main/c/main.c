c03facfea7217386bd432c4875cd8cff0620be0b
Luis Miguel Mejía Suárez <lmejias@psl.com.co>
2019-04-10 11:28:35 -0500

    Adding base main

---
+++ src/main/c/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "data.h"
+#include "functions.h"
+
+/**
+ * Main method - All code logic runs here.
+ */
+int main(int argc, const char* argv[]) {
+  printf("Hello, World!");
+  return 0;
+}
