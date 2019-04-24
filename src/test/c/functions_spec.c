89a079858e5154b569a1036b01f2e2c82c39be73
Vincent A. Arcila L <vaarcilal@eafit.edu.co>
2019-04-23 21:35:00 -0500

    added compute_acceleration tests

--- src/test/c/functions_spec.c
+++ src/test/c/functions_spec.c
@@ -5,7 +5,7 @@
 #include "functions.h"
 
 // Maximum acceptable error when comparing double values.
-#define TOLERANCE 0.05d
+#define TOLERANCE 0.00005d
 
 // Terminal color constants.
 #define RED   "\033[1;31m"
@@ -68,10 +68,45 @@ void test_sum_vectors_multiple_elements() {
 
   sum_vectors(size, v1, v2, result);
 
-  Vector expected[size] = { { 10.0d, 10.0d }, { -12.6d, 4.56d }, { -2.0d, 8.0d } };
+  Vector expected[size] = { { 10.0d, 10.0d }, { -12.61d, 4.56d }, { -2.0d, 8.0d } };
   for (int i = 0; i < size; i++) {
     for_assert(result[i].x_component, expected[i].x_component, "test_sum_vectors_multiple_elements - x_component", i);
     for_assert(result[i].y_component, expected[i].y_component, "test_sum_vectors_multiple_elements - y_component", i);
+  }
+  #undef size
+}
+
+/**
+ * Checks that the compute_acceleration function works for arrays of one element.
+ */
+void test_compute_acceleration_one_element() {
+  #define size 1
+  Vector forces[size] = { { 30, 30 } };
+  ParticleProperties particle_properties[size] = { { 3 } };
+  Vector resultant_acceleration[size] = { { 0 } };
+
+  compute_acceleration(size, particle_properties, forces, resultant_acceleration);
+
+  assert(resultant_acceleration[0].x_component, 10.0d, "test_compute_acceleration - x_component");
+  assert(resultant_acceleration[0].y_component, 10.0d, "test_compute_acceleration - y_component");
+  #undef size
+}
+
+/**
+ * Checks that the compute_acceleration function works for arrays of multiple elements.
+ */
+void test_compute_acceleration_multiple_elements() {
+  #define size 3
+  Vector forces[size] = { { -12.58, -15.896 }, { 13.945, -200.826 }, { -543.62, -0.62 } };
+  ParticleProperties particle_properties[size] = { { 0.367 }, { 3.967 }, { 0.52 } };
+  Vector resultant_acceleration[size] = { { 0 } };
+
+  compute_acceleration(size, particle_properties, forces, resultant_acceleration);
+
+  Vector expected[size] = { { -34.2779d, -43.31335149863761d }, { 3.5152508192588856d, -50.6241d }, { -1045.4231d, -1.1923d } };
+  for (int i = 0; i < size; i++) {
+    for_assert(resultant_acceleration[i].x_component, expected[i].x_component, "test_compute_acceleration_multiple_elements - x_component", i);
+    for_assert(resultant_acceleration[i].y_component, expected[i].y_component, "test_compute_acceleration_multiple_elements - y_component", i);
   }
   #undef size
 }
@@ -87,6 +122,8 @@ int main(void) {
   // Execute all tests.
   test_sum_vectors_one_element();
   test_sum_vectors_multiple_elements();
+  test_compute_acceleration_one_element();
+  test_compute_acceleration_multiple_elements();
 
   // If, at least one test failed, exit with an error code.
   return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
