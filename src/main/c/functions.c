e5d72e3ba18bf65d92ed6aad6452578cfe71bd5a
Vincent A. Arcila L <vaarcilal@eafit.edu.co>
2019-04-29 21:45:00 -0500

    Synchronized with master

--- src/main/c/functions.c
+++ src/main/c/functions.c
@@ -1,5 +1,28 @@
+#include <math.h>
+#include <stdlib.h>
 #include "data.h"
 #include "functions.h"
+
+/**
+ * Returns the size of a triangular matrix, without the diagonal.
+ */
+int size_triangular_matrix(const int n) {
+  return ((n - 1) * n) / 2;
+}
+
+/**
+ * Computes the overlap between two particles.
+ * Note: If the overlap is negative, there is no overlap.
+ */
+double compute_overlap(const Particle p1, const Particle p2) {
+  double d = p1.radious + p2.radious;
+
+  double x_diff = p1.x_coordinate - p2.x_coordinate;
+  double y_diff = p1.y_coordinate - p2.y_coordinate;
+  double distance = sqrt((x_diff * x_diff) + (y_diff * y_diff));
+
+  return d - distance;
+}
 
 /**
  * Takes two arrays of vectors, and sums together the vectors with the same index.
@@ -18,9 +41,40 @@ void compute_acceleration(const int size, const ParticleProperties *particles, c
   }
 }
 
+/**
+ * Derives the resultant velocity,
+ * of an initial velocity with an applied acceleration for given a time delta.
+ */
 void compute_velocity(const int size, const Vector *accelerations, const double dt, Vector *velocities){
   for (int i = 0; i < size; ++i) {
     velocities[i].x_component = velocities[i].x_component + accelerations[i].x_component * dt;
     velocities[i].y_component = velocities[i].y_component + accelerations[i].y_component * dt;
   }
+}
+
+/**
+ * Computes the contacts between all particles.
+ * Returns the number of contacts written on the buffer.
+ * Note: The size of the buffer should be equals to
+ *       the size of a triangular matrix for the number of particles.
+ */
+int compute_contacts(const int size, const Particle *particles, Contact *contacts_buffer) {
+  int k = 0;
+  double overlap;
+  Particle p1;
+  Particle p2;
+  for (int i = 0; i < size; ++i) {
+    for (int j = i + 1; j < size; ++j) {
+      p1 = particles[i];
+      p2 = particles[j];
+      overlap = compute_overlap(p1, p2);
+      if (overlap > 0) {
+        contacts_buffer[k].p1_idx = i;
+        contacts_buffer[k].p2_idx = j;
+        contacts_buffer[k].overlap = overlap;
+        ++k;
+      }
+    }
+  }
+  return k;
 }
