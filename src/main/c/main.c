5aac02800bbde1bf36bb5c5f8759aa34b74d03e3
Luis Miguel Mejía Suárez <lmejias@psl.com.co>
2019-05-08 11:51:26 -0500

    Adding the simulation loop to the main

--- src/main/c/main.c
+++ src/main/c/main.c
@@ -1,3 +1,4 @@
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include "data.h"
@@ -41,13 +42,33 @@ void free_all() {
 }
 
 /**
+ * Executes one step of the simulation.
+ */
+void simulation_step(const size_t particles_size, const double dt) {
+  size_t contact_size = compute_contacts(particles_size, particles, contacts_buffer);
+  //compute_forces(particles_size, contacts_size, contacts_buffer, particles, properties, velocities, dt, forces);
+  compute_acceleration(particles_size, properties, forces, accelerations);
+  compute_velocity(particles_size, accelerations, dt, velocities);
+  compute_displacement(particles_size, dt, velocities, displacements);
+  displace_particles(particles_size, displacements, particles);
+}
+
+/**
  * Main method - All code logic runs here.
  */
 int main(void) {
   // The simulation size is equals to the number of particles.
   const size_t simulation_size = 100;
-  const double dt = 0.000025;
   initialize(simulation_size);
+  // The simulation time and dt determine the maximum number of steps to execute.
+  const double simulation_time = 10;
+  const double dt = 0.000025;
+  const int max_steps = ceil(simulation_time / dt);
+
+  // Run the simulation until the max number of steps is reached.
+  for (int step = 0; step < max_steps; ++step) {
+    simulation_step(simulation_size, dt);
+  }
 
   // Free all memory resources and exit.
   free_all();
