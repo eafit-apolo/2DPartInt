e11881b8c2b6eaf194df2c6e8cd914da3fbe3e31
mherreral <mherreral@eafit.edu.co>
2020-10-19 13:42:11 -0500

    Changing files from csv form to binary form

--- src/cpp/csv.cpp
+++ src/cpp/csv.cpp
@@ -1,6 +1,7 @@
 #include <cstdlib>
 #include <fstream>
 #include <string>
+#include <iostream>
 extern "C" {
   #include "data.h"
 }
@@ -25,15 +26,27 @@ void write_simulation_step(const size_t num_particles, const Particle *particles
   // Open the csv file to write.
   std::ofstream output_file;
   output_file.open(
-    std::string(folder) + "/2DPartInt-Out.csv." + std::to_string(step),
-    std::ios_base::out | std::ios_base::trunc
+    std::string(folder) + "/2DPartInt-Out.bin." + std::to_string(step),
+    std::ios::out | std::ios::binary
   );
 
   // Write the header.
   output_file << "x coord, y coord, z coord, radius\n";
 
-  // Write the current status of each particle.
   for (size_t i = 0; i < num_particles; ++i) {
+    //char* coordinate = particles[i].x_coordinate' + '-' + particles[i].y_coordinate
+    //+ '-' + '0' + '-' + particles[i].radius + '\n';
+    output_file.write((char*)&particles[i].x_coordinate, sizeof(particles[i].x_coordinate));
+    output_file.write((char*)&", ", sizeof(", "));
+    output_file.write((char*)&particles[i].y_coordinate, sizeof(particles[i].y_coordinate));
+    output_file.write((char*)&", ", sizeof(", "));
+    output_file.write((char*)&"0", sizeof("0"));
+    output_file.write((char*)&", ", sizeof(", "));
+    output_file.write((char*)&particles[i].radius, sizeof(particles[i].radius));
+    output_file.write((char*)&"\n", sizeof("\n"));
+  }
+  // Write the current status of each particle.
+/*  for (size_t i = 0; i < num_particles; ++i) {
     output_file << particles[i].x_coordinate
                 << ", "
                 << particles[i].y_coordinate
@@ -42,7 +55,7 @@ void write_simulation_step(const size_t num_particles, const Particle *particles
                 << ", "
                 << particles[i].radius
                 << "\n";
-  }
+  }*/
 
   // Close the CSV file.
   output_file.close();
