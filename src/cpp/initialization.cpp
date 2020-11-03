eabcec1f124c3b34b5b3ced330c35043fbb08d5e
Vincent Alejandro Arcila Larrea <jandro.vins@gmail.com>
2020-11-02 20:13:20 -0500

    Adapted to new data structure

--- src/cpp/initialization.cpp
+++ src/cpp/initialization.cpp
@@ -18,6 +18,7 @@ extern Vector *forces;
 extern Vector *accelerations;
 extern Vector *velocities;
 extern Vector *displacements;
+extern Particle **grid;
 
 #ifndef M_PI
   #define M_PI 3.141592653589793
@@ -43,9 +44,9 @@ size_t initialize(const Config *config) {
   const double diameter = 2 * config->radius;
   const double mass = compute_mass(config);
 
-  // Maximum of particles on each limit
-  const unsigned int max_in_x = floor(config->x_limit / diameter);
-  const unsigned int max_in_y = floor(config->y_limit / diameter);
+  // Maximum of particles on each coordinate
+  const unsigned int max_in_x = config->x_particles;
+  const unsigned int max_in_y = config->y_particles;
 
   // The number of particles is equal to the product
   // of the particles that can fill each dimension,
@@ -55,21 +56,25 @@ size_t initialize(const Config *config) {
   // Allocate the memory for all the data structures.
   particles = (Particle*) calloc(num_particles, sizeof(Particle));
   properties = (ParticleProperties*) calloc(num_particles, sizeof(ParticleProperties));
-  contacts_buffer = (Contact*) calloc(size_triangular_matrix(num_particles), sizeof(Contact));
+  contacts_buffer = (Contact*) calloc(num_particles * num_particles, sizeof(Contact));
   normal_forces = (double*) calloc(num_particles * num_particles, sizeof(double));
   tangent_forces = (double*) calloc(num_particles * num_particles, sizeof(double));
   forces = (Vector*) calloc(num_particles, sizeof(Vector));
   accelerations = (Vector*) calloc(num_particles, sizeof(Vector));
   velocities = (Vector*) calloc(num_particles, sizeof(Vector));
   displacements = (Vector*) calloc(num_particles, sizeof(Vector));
+  grid = (Particle**) calloc(config->x_squares * config->y_squares, sizeof(Particle*)); // Squares in grid will be of lenght 2 * diameter
 
+
+  double shift = config->x_particles * config->radius; // Shift to the left so there is simmetry around 0 in x coordinates
   // Initialize the particles.
-  double x = config->radius;
+  double x = config->radius - shift;
   double y = config->radius;
   for (size_t i = 1; i < num_particles; ++i) {
     particles[i].x_coordinate = x;
     particles[i].y_coordinate = y;
     particles[i].radius = config->radius;
+    particles[i].idx = i;
     properties[i].mass = mass;
     properties[i].kn = config->kn;
     properties[i].ks = config->ks;
@@ -77,7 +82,7 @@ size_t initialize(const Config *config) {
     // Check if this particle is the last one for this row...
     if ((i % max_in_x) == 0) {
       // If it is, reset the x value and increment y.
-      x = config->radius;
+      x = config->radius - shift;
       y += diameter;
     } else {
       // If not, increment the x value.
@@ -86,9 +91,10 @@ size_t initialize(const Config *config) {
   }
 
   // Initialize the falling particle.
-  particles[0].x_coordinate = config->x_limit / 2;
-  particles[0].y_coordinate = config->y_limit + (4 * config->radius);
+  particles[0].x_coordinate = 0;
+  particles[0].y_coordinate = (config->y_particles * 2 * config->radius) + (4 * config->radius);
   particles[0].radius = config->radius;
+  particles[0].idx = 0;
   properties[0].mass = mass;
   properties[0].kn = config->kn;
   properties[0].ks = config->ks;
