a65270cbc31fb1972e25dfd0c5fc24a96b898701
Vincent Arcila <jandro.vins@gmail.com>
2020-11-08 23:15:31 -0500

    Well commented version of collisions.c and collisions.h

--- src/c/collisions.c
+++ src/c/collisions.c
@@ -1,23 +1,14 @@
 #include <math.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include <stddef.h>
 #include "data.h"
 #include "functions.h"
 #include "collisions.h"
 
 /**
- * Given a point (x, y), find the index for the square that should contain it.
+ * Helper function. Return the column number given an x coordinate. If the x coordinate lies outside the x dimension covered by the grid,
+ * then return a negative number: -2 for the left boundary and -1 for the right boundary.
  */
-int find_square(const int x, const int y, const int x_squares, const int y_squares, const double square_length){
-    int row_ind = find_row(y,y_squares,square_length);
-    int col_ind = find_col(x,x_squares,square_length);
-    if(col_ind < 0 || row_ind < 0){
-        return -1;
-    }
-    return row_ind * x_squares + col_ind;
-}
-
-int find_col(const int x, const int x_squares, const double square_length){
+int find_col(const double x, const int x_squares, const double square_length){
     double x_left_limit = -(x_squares*square_length/2); // The far leftest (if that word exists) of the grid
     double diff = x_left_limit -x;
     if(diff>0) return -2; // outside the left boundary
@@ -26,38 +17,63 @@ int find_col(const int x, const int x_squares, const double square_length){
     return col_ind;
 }
 
-int find_row(const int y, const int y_squares, const double square_length){
+/**
+ * Helper function. Return the row number given an y coordinate. If the y coordinate lies below 0,
+ * then return a negative number: -2 for the left boundary and -1 for the right boundary.
+ */
+int find_row(const double y, const int y_squares, const double square_length){
     int row_ind = floor(((double)y)/square_length);
     if(row_ind >= y_squares) return -1;
     if(row_ind < 0) return -2;
     return row_ind;
 }
 
-size_t compute_contacts(Particle** grid, const int x_squares, const int y_squares, const double square_length, Contact* contacts){
+/**
+ * Helper function. Given a point (x, y), find the index for the square that should contain it. If either coordinate lies outide the area covered
+ * by the grid, then return -1.
+ */
+int find_square(const double x, const double y, const int x_squares, const int y_squares, const double square_length){
+    int row_ind = find_row(y,y_squares,square_length);
+    int col_ind = find_col(x,x_squares,square_length);
+    if(col_ind < 0 || row_ind < 0){
+        return -1;
+    }
+    return row_ind * x_squares + col_ind;
+}
+
+/**
+ * From the Grid, find all the pairs of particles that are colliding with each other, create a struct Contact for
+ * each one and save it into 'contacts'. Returns the number of collisions.
+ *
+ * The search for contacts is done as following:
+ *     1. Traverse each square in the grid. If it is empty (there is a null pointer in grid[square_idx]), then continue to the next square.
+ *          2. Iterate each particle inside the square. The particle iterated over will be 'p'.
+ *              3. If p is the only particle inside the square, continue to 5.
+ *                  4. Traverse all the square to find collisions with its  square-neighbors.
+ *              5. Traverse the particles that are inside squares that could be containing particles colliding with p. The squares can be found with the following method:
+ *                  a. Imagine a circle with twice the radius of p, called neighboring cicle. Every other coliding particle's center 
+ *                     must be inside the neighboring circle, since all have the same radius. 
+ *                  b. We have to find the set of squares such that part of its geometric area is overlaping with p's neighboring circle.
+ *                     We do not do this, but something similar. Instead of using a circle, we use the square in which the neighboring circle is inscribed.
+ *                  c. We find the rows and columns that contain the limits (upper, lower, left and right) of said square. These will define
+ *                     us the set of squares inside Grid that could contain p's colliding particles.
+ *                  d. Traverse all the particles inside each found square to find collisions.
+ */
+size_t compute_contacts(Particle const *const *const grid, const int x_squares, const int y_squares, const double square_length, Contact* contacts){
     size_t k = 0; // current number of contacts
-    for(size_t row=0; row<y_squares; row++){
-        for(size_t col=0; col<x_squares; col++){
+    // For each square
+    for(int row=0; row<y_squares; row++){
+        for(int col=0; col<x_squares; col++){
             size_t square_idx = row*x_squares+col;
-            //printf("\n");
-            if(!grid[square_idx]) continue;
-            Particle* first = grid[square_idx];
-            Particle* p = grid[square_idx];
+            if(!grid[square_idx]) continue; // If the square is empty
+            Particle* first = (Particle*)grid[square_idx]; // Cast so the compiler does not yell because of referencing a const pointer
+            Particle* p =(Particle*)grid[square_idx]; // Iterator. Cast so the compiler does not yell because of referencing a const pointer
             while(p){
-                // First compare with particles within the same square
                 Particle* other = first;
-                while(other){
+                while(other){ // First compare with particles within the same square
                     if(other != p){
-                        if(!other){
-                            //printf("\n\nother is null in SAME SQUARE\n\n");
-                        }
-                        if(!p){
-                            //printf("\n\np is null in SAME SQUARE\n\n");
-                        }
-                        //printf("in SAME SQUARE  - p: %d , radius: %f - other: %d , radius: %f\n", p->idx, p->radius,  other->idx, other->radius);
                         const double overlap = compute_overlap(p, other);
-                        //printf("OVERLAP: %f\n", overlap);
                         if(overlap > 0){
-                            //printf("INSIDE > )\n");
                             contacts[k].p1_idx = p->idx;
                             contacts[k].p2_idx = other->idx;
                             contacts[k].overlap = overlap;
@@ -66,6 +82,8 @@ size_t compute_contacts(Particle** grid, const int x_squares, const int y_square
                     }
                     other = other->next;
                 }
+                // Then, compare with p's surrounding squares
+                // Find all the squares which could contain particles colliding with p
                 int left_col = find_col(p->x_coordinate - p->radius*2, x_squares, square_length);
                 if(left_col == -2) left_col = 0;
                 int right_col = find_col(p->x_coordinate + p->radius*2, x_squares, square_length);
@@ -74,22 +92,14 @@ size_t compute_contacts(Particle** grid, const int x_squares, const int y_square
                 if(bottom_row==-2) bottom_row = 0;
                 int top_row = find_row(p->y_coordinate + p->radius*2, y_squares, square_length);
                 if(top_row==-1) top_row=y_squares-1;
-
+                // Iterate over the squares
                 for(int neighbor_row=bottom_row; neighbor_row<=top_row; neighbor_row++){
                     for(int neighbor_col=left_col; neighbor_col<=right_col;neighbor_col++){
                         size_t neighbor_square_idx = neighbor_row*x_squares+neighbor_col;
-                        other = grid[neighbor_square_idx];
-                        //printf("\nCOLLGRID: %ld\n", neighbor_square_idx);
-                        if(other==NULL) continue;
-                        if(neighbor_square_idx==square_idx) continue;
+                        other = (Particle *)grid[neighbor_square_idx]; // Cast so the compiler does not yell because of referencing a const pointer
+                        if(other==NULL) continue; // If there are no particles inside this square
+                        if(neighbor_square_idx==square_idx) continue; // If this is p's square, then this has already been traversed
                         while(other){
-                            if(other==NULL){
-                                //printf("\n\nother is null in NEIGHBORS\n\n");
-                            }
-                            if(p==NULL){
-                                //printf("\n\np is null in NEIGHBORS\n\n");
-                            }
-                            //printf("in NEIGHBORS  - p: %d , radius: %f - other: %d , radius: %f\n", p->idx, p->radius,  other->idx, other->radius);
                             const double overlap = compute_overlap(p, other);
                             if(overlap > 0){
                                 contacts[k].p1_idx = p->idx;
@@ -98,7 +108,6 @@ size_t compute_contacts(Particle** grid, const int x_squares, const int y_square
                                 k++;
                             }
                             other = other->next;
-                            //printf("\nPOINTER: %p", (void*)other);
                         }
                     }
                 }
@@ -113,50 +122,22 @@ size_t compute_contacts(Particle** grid, const int x_squares, const int y_square
  * Fills the grid with particles. The side length of each square grid is twice the diameter of each particle,
  * since all particles have the same radius. Running time is O(N), and memory space is O(N) but really 2*N because of 'lasts' pointers array.
  */
-void fill_grid(const int num_particles, Particle* particles, Particle** grid, const int x_squares, const int y_squares,
-        const double square_length){
-    double x_left_limit = -(x_squares*square_length/2); // The far leftest (if that word exists) of the grid
-    Particle** lasts = (Particle**) calloc(x_squares * y_squares, sizeof(Particle*)); // Array of pointers to last positions
-
-    // For each particle, find its square. Keep in mind that grid is an array of  pointers to the first element in the squares, and that
-    // lasts is an array of pointers to the last element in the squares.
-    int counter = 0;
-    for(int i=0; i<num_particles; i++){
-        Particle* p = &particles[i];
+void fill_grid(const size_t num_particles, const int x_squares, const int y_squares, const double square_length,
+        Particle const *const particles, Particle* *const grid, Particle* *const grid_lasts){
+    // For each particle, find its grid's square. Keep in mind that grid is an array of pointers to the first element in the squares, and that
+    // grid_lasts is an array of pointers to the last element in the squares.
+    for(size_t i=0; i<num_particles; i++){
+        Particle* p = (Particle*) &particles[i]; // Cast so the compiler does not yell because of referencing a const pointer
         p->next=NULL; // Make sure we do not do anything funny
         int square_ind = find_square(p->x_coordinate, p->y_coordinate, x_squares, y_squares, square_length);
         if(square_ind< 0) continue; // The particle is outside the boundaries of our grid
-        counter++;
-        //printf("FILLGRID:%d\n", square_ind);
-
-        if(!lasts[square_ind]){ // if is the first element in the square
+        if(!grid_lasts[square_ind]){ // if is the first element in the square
             grid[square_ind] = p;
-            lasts[square_ind] = p;
+            grid_lasts[square_ind] = p;
         }else{
-            lasts[square_ind]->next=p;
-            lasts[square_ind] = p;
+            grid_lasts[square_ind]->next=p;
+            grid_lasts[square_ind] = p;
         }
     }
 
-    free(lasts);
-}
-
-
-int print_grid(Particle** grid, const int x_squares, const int y_squares, int iter_num){
-    int total_particles = 0;
-    printf("STEP: %d\n", iter_num);
-    for(int i=0; i < x_squares*y_squares;i++){
-        if(!grid[i]) continue;
-        Particle* p = grid[i];
-        int counter = 1;
-        while(p->next){
-            p = p->next;
-            counter++;
-        }
-        total_particles+=counter;
-        if(counter!=0)
-        printf("G%d: %d , ", i, counter);
-    }
-    printf("\nTOTAL: %d\n\n", total_particles);
-    return total_particles;
 }
